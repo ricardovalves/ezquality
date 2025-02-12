ff26183fb4023dde2b95a18d7272ef9cfa8be9cc
ricardovalves <ricardovalves@gmail.com>
2025-02-12 19:44:28 +0000

    Added SidebarProvider with simple hello world

---
+++ src/SidebarProvider.ts
@@ -0,0 +1,71 @@
+import * as vscode from "vscode";
+
+export class SidebarProvider implements vscode.WebviewViewProvider {
+  _view?: vscode.WebviewView;
+
+  constructor(private readonly _extensionUri: vscode.Uri) {}
+
+  public resolveWebviewView(webviewView: vscode.WebviewView) {
+    this._view = webviewView;
+
+    webviewView.webview.options = {
+      // Allow scripts in the webview
+      enableScripts: true,
+
+      localResourceRoots: [this._extensionUri],
+    };
+
+    webviewView.webview.html = this._getHtmlForWebview(webviewView.webview);
+
+    webviewView.webview.onDidReceiveMessage(async (data) => {
+      switch (data.type) {
+        case "onInfo": {
+          if (!data.value) {
+            return;
+          }
+          vscode.window.showInformationMessage(data.value);
+          break;
+        }
+        case "onError": {
+          if (!data.value) {
+            return;
+          }
+          vscode.window.showErrorMessage(data.value);
+          break;
+        }
+      }
+    });
+  }
+
+  public revive(panel: vscode.WebviewView) {
+    this._view = panel;
+  }
+
+  private _getHtmlForWebview(webview: vscode.Webview) {
+    // const styleResetUri = webview.asWebviewUri(
+    //   vscode.Uri.joinPath(this._extensionUri, "media", "reset.css")
+    // );
+    // const scriptUri = webview.asWebviewUri(
+    //   vscode.Uri.joinPath(this._extensionUri, "out", "compiled/sidebar.js")
+    // );
+    // const styleMainUri = webview.asWebviewUri(
+    //   vscode.Uri.joinPath(this._extensionUri, "out", "compiled/sidebar.css")
+    // );
+    // const styleVSCodeUri = webview.asWebviewUri(
+    //   vscode.Uri.joinPath(this._extensionUri, "media", "vscode.css")
+    // );
+
+    // // Use a nonce to only allow a specific script to be run.
+    // const nonce = getNonce();
+
+    return `<!DOCTYPE html>
+			<html lang="en">
+			<head>
+				<meta charset="UTF-8">
+				<meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <body>
+                <h1>Hello World</h1>
+			</body>
+			</html>`;
+  }
+}
